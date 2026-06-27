9b4ef827b8e90083e8494839d248b2efb389d9eb
Vedant <your-yadaovedant@gmail.com>
2026-06-27 19:55:01 +0530

    Function : Created As analyse string

---
+++ Assignment12/Q2.py
@@ -0,0 +1,8 @@
+# Function : Created As analyse string
+def analyze_string(s):
+    # Handle empty string case
+    if s == "":
+        print("Empty string provided. Nothing to analyze.")
+        return
+    
+
